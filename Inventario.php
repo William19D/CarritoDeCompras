ad561334dec87110dd2e9b2a43449d3e93b32285
Daniel <88367009+William19D@users.noreply.github.com>
2024-11-21 00:34:33 -0500

    actualizacion de clase Inventario.php

--- Inventario.php
+++ Inventario.php
@@ -8,9 +8,27 @@ class Inventario {
 
     public function generarListaDesplegable() {
         $sql = $this->conexion->query("SELECT concepto FROM detalleproducto");
-        echo '<select class="form-control" name="concepto">';
+        echo '<select class="form-control" name="detalle">';
         while ($row = $sql->fetch_assoc()) {
             echo '<option value="' . $row['concepto'] . '">' . $row['concepto'] . '</option>';
+        }
+        echo '</select>';
+    }
+
+    public function generarListaDesplegableIdCiudadano() {
+        $sql = $this->conexion->query("SELECT id FROM ciudadano");
+        echo '<select class="form-control" name="idCiudadano">';
+        while ($row = $sql->fetch_assoc()) {
+            echo '<option value="' . $row['id'] . '">' . $row['id'] . '</option>';
+        }
+        echo '</select>';
+    }
+
+    public function generarListaDesplegableIdProducto() {
+        $sql = $this->conexion->query("SELECT id FROM producto");
+        echo '<select class="form-control" name="idProducto">';
+        while ($row = $sql->fetch_assoc()) {
+            echo '<option value="' . $row['id'] . '">' . $row['id'] . '</option>';
         }
         echo '</select>';
     }
