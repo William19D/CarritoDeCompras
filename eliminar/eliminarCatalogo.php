c0859c9adc8964507b24ecde636ac8bb5d0b835e
Cristian Alejandro Sierra Beermudez <80225020+Casb29XD@users.noreply.github.com>
2024-11-20 22:44:34 -0500

    lol
    lol

--- eliminar/eliminarCatalogo.php
+++ eliminar/eliminarCatalogo.php
@@ -1,10 +1,10 @@
 <?php
-      include "conexion.php";
+      include "../conexion.php";
       if(!empty($_GET["id"])){
             $id = $_GET["id"];
             $sql= $conexion->query("DELETE FROM catalogopreferencias WHERE id=$id");
             if($sql ==1){
-                  header("location:index.php");
+                  header("location:/index.php");
             }else{
                   echo '<div class="alert alert-success">Problemas a la hora de eliminar</div>';
             }
--- eliminar/eliminarCliente.php
+++ eliminar/eliminarCliente.php
@@ -1,10 +1,10 @@
 <?php
-      include "conexion.php";
+      include "../conexion.php";
       if(!empty($_GET["id"])){
             $id = $_GET["id"];
-            $sql= $conexion->query("DELETE FROM ciudadano WHERE id=$id");
+            $sql= $conexion->query("DELETE FROM ciudadano WHERE cedula=$id");
             if($sql ==1){
-                  header("location:index.php");
+                  header("location:/index.php");
             }else{
                   echo '<div class="alert alert-success">Problemas a la hora de eliminar</div>';
             }
