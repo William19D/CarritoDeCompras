b5aba048c2fd311304118d3828aa34f69ae4b02c
Cristian Alejandro Sierra Beermudez <80225020+Casb29XD@users.noreply.github.com>
2024-11-21 00:15:41 -0500

    lol
    lol

--- carroCompras.php
+++ carroCompras.php
@@ -157,7 +157,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['btnGuardar'])) {
                 <td><?= $datos->total ?></td>
                 <td>
                     <a href="actualizar.php?id=<?= $datos->id ?>" class="btn btn-small btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
-                    <a href="eliminar/eliminarCarrito.php?=<?= $datos->codigo ?>" class="btn btn-small btn-danger"><i class="fa-solid fa-eraser"></i></a>
+                    <a href="eliminar/eliminarCarrito.php?=<?= $datos->id ?>" class="btn btn-small btn-danger"><i class="fa-solid fa-eraser"></i></a>
                 </td>
             </tr>
         <?php } ?>
